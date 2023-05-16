33ebe7aed862ae6f84da02df957e8ce12815e9f4
kimminki <kimminki02@naver.com>
2023-05-16 11:41:00 +0900

    feat : Header 구현, (Login, Logout 기능 구현), 햄버거 바 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Outlet />
       <Footer />
     </>
